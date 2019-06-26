01301e1e06e32e6bd29811bd3e04f9586b6ea087
Aniruddha Karajgi <akarajgi0@gmail.com>
2019-06-26 22:53:05 +0530

    Implement compute_timeseries and Empty and Merge Exclude classes
    This patch implements compute_timeseries in the
    Metric class using resampling. It has been moved
    from individual metric files.
    
    This patch also adds EmptyExclude and
    MergeExclude classes. 
     
    Signed-off-by: Aniruddha Karajgi <akarajgi0@gmail.com>

--- implementations/code_df/metric.py
+++ implementations/code_df/metric.py
@@ -1,8 +1,9 @@
 import pandas as pd
 
+
 class Metric():
     """
-    Create a dataframe from items fetched by Perceval
+    Create a DataFrame from items fetched by Perceval
 
     All classes computing metrics based on data frames
     will be descendents of this class.
@@ -33,3 +34,45 @@ class Metric():
         """
 
         raise NotImplementedError
+
+    def _aggregate(self, series):
+        """
+        Aggregate the re-sampled DataFrame
+        """
+
+        raise NotImplementedError
+
+    def compute_time_series(self, cols=None, period='M'):
+        """
+        The metric value is computed for each fixed interval of time
+        from the "since" date to the "until" date arguments, specified
+        during object initiation.
+
+        The fixed time interval can be either a month or a week.
+
+        :param cols: A list of columns on which aggregation will occur.
+            In some cases, no particular column is required, for example,
+            to get count. In this case, cols is set to the "category"
+            column.
+
+        :param period: A string which can be any one of the pandas time
+        series rules:
+            'W': week
+            'M': month
+            'D': day
+
+        :returns df: A DataFrame whose rows each represent an interval
+            of "period" and the count for that interval
+        """
+
+        df = self.df
+
+        df = df.set_index('created_date')
+        # No particular column to aggregate on:
+        if cols is None:
+            cols = ['category']
+
+        df = df[cols].resample(period).agg([self._aggregate])
+        df.columns = df.columns.droplevel(0)
+
+        return df
