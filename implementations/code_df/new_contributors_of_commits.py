7be82efdca4d68c108b3fa84b931fca6c105c31a
Aniruddha Karajgi <akarajgi0@gmail.com>
2019-06-28 22:48:39 +0530

    Update to recent changes
    This patch makes the notebook and python script for
    new contributors of commits compatible to recent
    changes. 
    
     This patch also:
    * Renames compute_time_series to
    time_series.
    
    * Improves the docstring for the _agg
    method.
    
    Signed-off-by: Aniruddha Karajgi <akarajgi0@gmail.com>

--- implementations/code_df/new_contributors_of_commits.py
+++ implementations/code_df/new_contributors_of_commits.py
@@ -1,24 +1,15 @@
 import pandas as pd
 from datetime import datetime
+
 import utils
-from is_source_code import IsSourceCode, Naive
+import conditions
 from commit import Commit
 
 
-class NewContributorsoFCommits(Commit):
+class NewContributorsOfCommits(Commit):
     """
     Class for New contributors of commits.
-
-    :param items: A list of dictionaries, each element a line from the
-        JSON file with Perceval data
-    :param date_range: A tuple which represents the start and end date of
-        interest
-    :param issrccode_obj: An object of IsSourceCode, used to determine
-        what comprises source code.
     """
-
-    def __init__(self, items, date_range=(None, None), issrccode_obj=None):
-        super().__init__(items, date_range, issrccode_obj)
 
     def compute(self, check_range=(None, None)):
         """
@@ -31,103 +22,118 @@ class NewContributorsoFCommits(Commit):
         :returns count: the number of new committers who committed
             between the dates of check_range
         """
+
         self.check_since, self.check_until = check_range
 
-        if self.check_since is None:
-            self.check_since = self.since
+        df = pd.DataFrame(columns=self.df.columns)
 
-        if self.check_until is None:
-            self.check_until = self.until
+        if self.check_since is not None:
+            df = self.df[self.df['created_date'] < self.check_since]
 
-        df = self.df[self.df['created_date'] < self.check_since]
-
-        committers_before_check_since = set()
         committers_before_check_since = set(df['author'].tolist())
-        count_new_committers = 0
+
         df_new_committers = self.df['author'][
-                    (self.check_since <= self.df['created_date'])
-                    & (self.check_until >= self.df['created_date'])
-                    & (~self.df['author']
+                    (~self.df['author']
                         .isin(
                             list(committers_before_check_since)))
                 ]
+
+        if self.check_since is not None:
+            df_new_committers = df_new_committers[
+                self.check_since <= self.df['created_date']]
+
+        if self.check_until is not None:
+            df_new_committers = df_new_committers[
+                self.check_until >= self.df['created_date']]
+
         count_new_committers = len(df_new_committers.unique())
         return count_new_committers
 
-    def compute_timeseries(self, period='month'):
+    def _agg(self, df, period):
         """
-        The metric value is computed for each fixed interval of time
-        from the "since" date to the "until" date arguments, specified
-        during object initiation.
+        Call an aggregation operation on a DataFrame or Series
+        to count the number of new committers in a period when
+        compared to the previous period.
 
-        The fixed time interval can be either a month or a week.
+        :param df: a pandas DataFrame on which the aggregation will be
+            applied.
 
-        :param period: A string which can be either "month" or "week"
+        :param period: A string which can be any one of the pandas time
+            series rules:
+            'W': week
+            'M': month
+            'D': day
 
-        :returns dataframe: A DataFrame whose rows each represent an interval
-            of "period" and the count for that interval
+        :returns df: The final aggregated DataFrame
         """
-        old_committers = set()
 
-        def count_new(series):
+        def __new_committers_count(series):
             """
-            An aggregate function that calculates only those values which
-            are new in a grouping as compared to a previous dataframe
-            grouping
+            Aggregation method to count the number of new committers
+            per period.
 
-            :param series: a pandas Series
+            The set of names of people who committed before the current
+            period is stored in old_committers and any new committers
+            in the current period whose names are not in old_committers
+            are counted.
 
-            :returns count: the number of new elements in series
+            :param series: a pandas DataFrame on which the aggregation will be
+                applied.
+
+            :param period: A string which can be any one of the pandas time
+                series rules:
+                'W': week
+                'M': month
+                'D': day
+
+            :returns count: The number of new committers
             """
-            nonlocal old_committers
+            if len(series) > 0:
+                old_committers = \
+                    set(
+                        self.df['author'][
+                            self.df['created_date'] < series.index[0]]
+                        .tolist())
 
-            count = len(series[~series.isin(list(old_committers))].unique())
-            old_committers.update(series.tolist())
-            return count
+                count = len(series[~series.isin(list(old_committers))]
+                            .unique())
+                old_committers.update(series.tolist())
+                return count
 
-        df = self.df
-        if period == 'month':
-            timeseries_series = df['author'] \
-                .groupby([df['created_date'].dt.year.rename('year'),
-                          df['created_date'].dt.month.rename('month')]) \
-                .agg(count_new)
+            else:
+                return 0
 
-            all_periods = pd.DataFrame(
-                            pd.date_range(self.since, self.until, freq='M'),
-                            columns=['Dates'])
-            all_periods = pd.DataFrame(
-                [all_periods['Dates'].dt.year.rename('year'),
-                 all_periods['Dates'].dt.month.rename("month")]).T
-
-        elif period == 'week':
-            timeseries_series = df['author'] \
-                .groupby([df['created_date'].dt.year.rename('year'),
-                          df['created_date'].dt.week.rename('week')])   \
-                .agg(count_new)
-
-            all_periods = pd.DataFrame(
-                            pd.date_range(self.since, self.until, freq='W'),
-                            columns=['Dates'])
-            all_periods = pd.DataFrame(
-                [all_periods['Dates'].dt.year.rename('year'),
-                 all_periods['Dates'].dt.week.rename('week')]).T
-
-        else:
-            raise ValueError("period parameter can take 'month' or 'week'")
-
-        timeseries_df = pd.DataFrame(timeseries_series)
-        timeseries_df.reset_index(inplace=True)
-        timeseries_df.columns = ['year', period, 'new_committers']
-        merged_df = all_periods.merge(timeseries_df, how='outer').fillna(0)
-
-        dataframe = merged_df
-        return dataframe
+        df = df.resample(period)['author'].agg([__new_committers_count])
+        return df
 
 
 if __name__ == "__main__":
-    date_since = datetime.strptime("2018-09-07", "%Y-%m-%d")
-    issourcecode = IsSourceCode(["tests/"], Naive)
-    items = utils.read_JSON_file('../git-commits.json')
-    new_committers = NewContributorsoFCommits(items, date_range=(date_since, None),
-                                       issrccode_obj=issourcecode)
-    print(new_committers.compute((date_since, None)))
+
+    date_since = datetime.strptime("2018-01-01", "%Y-%m-%d")
+    date_until = datetime.strptime("2018-07-01", "%Y-%m-%d")
+    check_since = datetime.strptime("2018-03-08", "%Y-%m-%d")
+    check_until = datetime.strptime("2018-06-08", "%Y-%m-%d")
+    items = utils.read_json_file('../git-commits.json')
+
+    new_committers = NewContributorsOfCommits(items)
+    print("New committers, total: {}".format(new_committers.compute()))
+
+    new_committers_interval = NewContributorsOfCommits(
+        items,
+        (date_since, date_until),
+        is_code=[conditions.DirExclude(['tests']),
+                 conditions.PostfixExclude(['.md', 'COPYING'])])
+
+    print("Variations in the number of new committers"
+          " between 2018-01-01 and 2018-07-01: ")
+    print(new_committers_interval.time_series(period='M'))
+
+    new_committers_dated = NewContributorsOfCommits(
+                                            items,
+                                            (date_since, date_until),
+                                            is_code=[
+                                                conditions.DirExclude(['tests']),
+                                                conditions.PostfixExclude(
+                                                    ['.md', 'COPYING'])])
+    print("New committers, after 2018-03-08 (excluding some files):",
+          new_committers.compute(check_range=(check_since, None)))
