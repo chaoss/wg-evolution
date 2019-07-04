ca933218ee8a0d007dcb8935193d7392e786883e
Aniruddha Karajgi <akarajgi0@gmail.com>
2019-07-05 01:23:24 +0530

    Re-implement with groupby author
    This patch allows for implementing the metric in a
     slightly different way  by grouping with respect to
    'author' column and performing a 'count' aggregation
    operation to compute the metric. 
     
    Signed-off-by: Aniruddha Karajgi <akarajgi0@gmail.com>

--- implementations/code_df/new_contributors_of_commits.py
+++ implementations/code_df/new_contributors_of_commits.py
@@ -8,8 +8,28 @@ from commit import Commit
 
 class NewContributorsOfCommits(Commit):
     """
-    Class for New contributors of commits.
-    """
+    Initilizes self.df, the dataframe with one commit per row.
+
+    :param items: A list of dictionaries.
+        Each item is a Perceval dictionary, obtained from a JSON
+        file or from Perceval directly.
+
+    :param date_range: A tuple which represents the period of interest
+        It is of the form (since, until), where since and until are strings.
+        Either, or both can be None. If, for example, since is None, that
+        would mean that all commits from the first commit to the commit
+        who last falls inside the until range will be included.
+
+    :param is_code:  list of CodeCondition objects
+        It is used to determine what comprises source code.
+        """
+
+    def __init__(self, items, date_range=(None, None),
+                 is_code=[conditions.Naive()], conds=[]):
+
+        super().__init__(items, date_range, is_code, conds)
+
+        self.df = self.df.loc[self.df.groupby('author')['created_date'].idxmin()]
 
     def compute(self, check_range=(None, None)):
         """
@@ -19,41 +39,30 @@ class NewContributorsOfCommits(Commit):
         :param check_range: A tuple which represents the start and end date
             when new committers will be considered
 
-        :returns count: the number of new committers who committed
+        :returns count_new_committers: the number of new committers who committed
             between the dates of check_range
         """
 
         self.check_since, self.check_until = check_range
 
-        df = pd.DataFrame(columns=self.df.columns)
+        df = self.df
 
         if self.check_since is not None:
-            df = self.df[self.df['created_date'] < self.check_since]
-
-        committers_before_check_since = set(df['author'].tolist())
-
-        df_new_committers = self.df['author'][
-                    (~self.df['author']
-                        .isin(
-                            list(committers_before_check_since)))
-                ]
-
-        if self.check_since is not None:
-            df_new_committers = df_new_committers[
-                self.check_since <= self.df['created_date']]
+            df = df[df['created_date'] < self.check_since]
 
         if self.check_until is not None:
-            df_new_committers = df_new_committers[
-                self.check_until >= self.df['created_date']]
+            df = df[df['created_date'] >= self.check_until]
 
-        count_new_committers = len(df_new_committers.unique())
+        count_new_committers = len(df.index)
         return count_new_committers
 
     def _agg(self, df, period):
         """
-        Call an aggregation operation on a DataFrame or Series
+        Perform an aggregation operation on a DataFrame or Series
         to count the number of new committers in a period when
-        compared to the previous period.
+        compared to committers before that period.
+
+        This method uses the 'count' aggregation method.
 
         :param df: a pandas DataFrame on which the aggregation will be
             applied.
@@ -67,43 +76,7 @@ class NewContributorsOfCommits(Commit):
         :returns df: The final aggregated DataFrame
         """
 
-        def __new_committers_count(series):
-            """
-            Aggregation method to count the number of new committers
-            per period.
-
-            The set of names of people who committed before the current
-            period is stored in old_committers and any new committers
-            in the current period whose names are not in old_committers
-            are counted.
-
-            :param series: a pandas DataFrame on which the aggregation will be
-                applied.
-
-            :param period: A string which can be any one of the pandas time
-                series rules:
-                'W': week
-                'M': month
-                'D': day
-
-            :returns count: The number of new committers
-            """
-            if len(series) > 0:
-                old_committers = \
-                    set(
-                        self.df['author'][
-                            self.df['created_date'] < series.index[0]]
-                        .tolist())
-
-                count = len(series[~series.isin(list(old_committers))]
-                            .unique())
-                old_committers.update(series.tolist())
-                return count
-
-            else:
-                return 0
-
-        df = df.resample(period)['author'].agg([__new_committers_count])
+        df = df.resample(period)['author'].agg(['count'])
         return df
 
 
@@ -115,7 +88,7 @@ if __name__ == "__main__":
     check_until = datetime.strptime("2018-06-08", "%Y-%m-%d")
     items = utils.read_json_file('../git-commits.json')
 
-    new_committers = NewContributorsOfCommits(items)
+    new_committers = NewContributorsOfCommits(items)    
     print("New committers, total: {}".format(new_committers.compute()))
 
     new_committers_interval = NewContributorsOfCommits(
