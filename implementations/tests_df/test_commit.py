306f38c4884b39ccc52e7e55e3d4d9754e0298a8
Aniruddha Karajgi <akarajgi0@gmail.com>
2019-07-11 00:55:47 +0530

    Add docstrings and minor tests
    This patch adds docstrings and minor changes to the tests. 
     
    Signed-off-by: Aniruddha Karajgi <akarajgi0@gmail.com>

--- implementations/tests_df/test_commit.py
+++ implementations/tests_df/test_commit.py
@@ -1,5 +1,7 @@
 import datetime
 import unittest
+import json
+
 import sys
 sys.path.append('..')
 from code_df.commit import Commit
@@ -8,7 +10,16 @@ from code_df import conditions
 from pandas.util.testing import assert_frame_equal
 
 
-def read_file(file_path):
+def read_file(path):
+    """
+    Given a line-by-line JSON file, this function converts it to
+    a Python dictionary and returns all such lines as a list.
+
+    :param path: the path to the JSON file
+
+    :returns items: a list of dictionaries read from the JSON file
+    """
+
     items = list()
     with open(path, 'r') as raw_data:
         for line in raw_data:
@@ -19,11 +30,29 @@ def read_file(file_path):
 
 
 class Test_filterout(unittest.TestCase):
+    """
+    Class to test the _filterout method, defined in the Commit class.
+    """
 
     def setUp(self):
+        """
+        Run before each test to read the test data file
+        """
+
         self.items = read_file('test_commits_data.json')
 
         class Temp(Commit):
+            """
+            The Temp class.
+
+            A temporary sub-class of the code_df.Commit class.
+
+            It overrides the __init__ method so that there is no
+            implicit call to the _filterout method while instantiating
+            an object of this class, which happens in the case of
+            the Commit class.
+            """
+
             def __init__(self, items, date_range=(None, None),
                          is_code=[conditions.Naive()], conds=[]):
 
@@ -89,7 +118,6 @@ class Test_filterout(unittest.TestCase):
 
         assert_frame_equal(expected_df, temp.df)
 
-
     def test__filterout_MergeExclude(self):
         """
         Test whether _filterout excludes merge
@@ -109,13 +137,20 @@ class Test_filterout(unittest.TestCase):
 
 
 class Test_flatten(unittest.TestCase):
+    """
+    Class to the _flatten method, defined in the Commit class.
+    """
 
     def setUp(self):
+        """
+        Run before each test to read the test data file
+        """
+
         self.items = utils.read_file('test_commits_data.json')
 
     def test__flatten_valid_input(self):
         """
-        Test for valid input. A commit that satsifies all conditions
+        Test for valid input. A commit that satisfies all conditions
         passed while creating the Commit object for testing. A properly
         flattened commit is expected.
         """
@@ -136,12 +171,15 @@ class Test_flatten(unittest.TestCase):
                 'refs': [],
                 'parents': [],
                 'files': [
-                {'action': 'A', 'added': '10', 'file': '.gitignore', 'indexes': ['0000000', 'ceaedd5'],
-                 'modes': ['000000', '100644'], 'removed': '0'},
-                {'action': 'A', 'added': '1', 'file': 'AUTHORS', 'indexes': ['0000000', 'a67f214'],
-                 'modes': ['000000', '100644'], 'removed': '0'},
-                {'action': 'A', 'added': '674', 'file': 'LICENSE', 'indexes': ['0000000', '94a9ed0'],
-                 'modes': ['000000', '100644'], 'removed': '0'}],
+                    {'action': 'A', 'added': '10', 'file': '.gitignore',
+                     'indexes': ['0000000', 'ceaedd5'],
+                        'modes': ['000000', '100644'], 'removed': '0'},
+                    {'action': 'A', 'added': '1', 'file': 'AUTHORS',
+                     'indexes': ['0000000', 'a67f214'],
+                        'modes': ['000000', '100644'], 'removed': '0'},
+                    {'action': 'A', 'added': '674', 'file': 'LICENSE',
+                     'indexes': ['0000000', '94a9ed0'],
+                        'modes': ['000000', '100644'], 'removed': '0'}],
                 'files_action': 3,
                 'merge': False
              }
@@ -175,8 +213,8 @@ class Test_flatten(unittest.TestCase):
         for index, item in expected_df.iterrows():
 
             valid_files = [file['file'] for file in item['files'] if
-                          all(condition.check(file['file'])
-                              for condition in commit.is_code)]
+                           all(condition.check(file['file'])
+                               for condition in commit.is_code)]
 
             if len(valid_files) == 0:
                 expected_df.drop(index, inplace=True)
@@ -196,8 +234,8 @@ class Test_flatten(unittest.TestCase):
         for index, item in expected_df.iterrows():
 
             valid_files = [file['file'] for file in item['files'] if
-                          all(condition.check(file['file'])
-                              for condition in commit.is_code)]
+                           all(condition.check(file['file'])
+                               for condition in commit.is_code)]
 
             if len(valid_files) == 0:
                 expected_df.drop(index, inplace=True)
