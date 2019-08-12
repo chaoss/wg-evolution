6832bed70127500e9b3d6074526a9ac33f1bb01d
Aniruddha Karajgi <akarajgi0@gmail.com>
2019-08-12 10:50:23 +0530

    Add tests for Issues New df implementation
    This patch adds tests for the Issues 
    New metric's pandas implementation.
     
    Signed-off-by: Aniruddha Karajgi <akarajgi0@gmail.com>

---
+++ implementations/tests/tests_df/test_issues_new_github.py
@@ -0,0 +1,64 @@
+import unittest
+import json
+from pandas.util.testing import assert_frame_equal
+
+from implementations.code_df.issues_new_github import IssuesNewGitHub
+
+
+def read_file(path):
+    """
+    Given a line-by-line JSON file, this function converts it to
+    a Python dictionary and returns all such lines as a list.
+
+    :param path: the path to the JSON file
+
+    :returns items: a list of dictionaries read from the JSON file
+    """
+
+    items = list()
+    with open(path, 'r') as raw_data:
+        for line in raw_data:
+            line = json.loads(line)
+
+            items.append(line)
+    return items
+
+
+class TestIssuesNewGitHub(unittest.TestCase):
+
+    def setUp(self):
+        """
+        Run before each test to read the test data file
+        """
+
+        self.items = read_file('data/test_issues_data.json')
+
+    def test_compute(self):
+        """
+        Test the compute method of a IssuesNewGitHub
+        object instantiated with default parameters.
+        """
+
+        issues = IssuesNewGitHub(self.items)
+        expected_count = 20
+        count = issues.compute()
+        self.assertEqual(expected_count, count)
+
+    def test__agg(self):
+        """
+        Test the _agg method of a IssuesNewGitHub
+        object with default parameters when re-sampling
+        on a weekly basis.
+        """
+
+        issues = IssuesNewGitHub(self.items)
+        issues.df = issues.df.set_index('created_date')
+        test_df = issues.df
+        test_df = test_df.resample('W')['category'].agg(['count'])
+
+        issues.df = issues._agg(issues.df, 'W')
+        assert_frame_equal(test_df, issues.df)
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
